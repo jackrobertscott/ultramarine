fea62498bf3baee34cc1fdfedbec78495821614c
Jack Scott <jack.rob.scott@gmail.com>
2018-12-02 20:34:15 +0800

    made attempts at trying to get something working

--- src/modules/Theme.ts
+++ src/modules/Theme.ts
@@ -24,26 +24,48 @@ export interface IChosen {
 }
 
 export default class Theme {
-  private config: IThemeConfig;
+  private base: ICSSProps;
+  private states: IStates;
+  private combos: ICombos;
   private chosen: IChosen;
 
-  constructor(config: IThemeConfig, chosen?: IChosen) {
-    expect.type('config', config, 'object');
-    expect.type('config.base', config.base, 'object');
-    expect.type('config.states', config.states, 'object', true);
-    expect.type('config.combos', config.combos, 'object', true);
-    expect.type('chosen', config, 'object', true);
-    this.config = { ...config };
-    this.chosen = chosen ? { ...chosen } : { states: [], combos: [] };
+  constructor({ base, states, combos }: IThemeConfig, chosen?: IChosen) {
+    expect.type('config.base', base, 'object', true);
+    expect.type('config.states', states, 'object', true);
+    expect.type('config.combos', combos, 'object', true);
+    this.base = base || {};
+    this.states = states || {};
+    this.combos = combos || {};
+    this.chosen = {
+      states: (chosen && chosen.states) || [],
+      combos: (chosen && chosen.combos) || [],
+    };
   }
 
-  public use(options: { states?: string; combos?: string }): Theme {
-    expect.type('options', options, 'object');
-    const chosen = {
-      states: [...this.chosen.states, ...this.digest(options.states)],
-      combos: [...this.chosen.combos, ...this.digest(options.combos)],
+  public render() {
+    return {
+      base: this.base,
+      states: this.states,
+      combos: this.combos,
+      chosen: this.chosen,
     };
-    return new Theme(this.config, chosen);
+  }
+
+  public use({ states, combos }: { states?: string; combos?: string }): Theme {
+    const chosen = {
+      states: [...this.chosen.states, ...this.digest(states)],
+      combos: [...this.chosen.combos, ...this.digest(combos)],
+    };
+    return this.clone(chosen);
+  }
+
+  private clone(chosen: IChosen): Theme {
+    const config = {
+      base: { ...this.base },
+      states: { ...this.states },
+      combos: { ...this.combos },
+    };
+    return new Theme(config, { ...(chosen || this.chosen) });
   }
 
   private digest(options?: string) {
